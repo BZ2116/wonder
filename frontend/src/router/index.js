bc523b5a3c177cd54911e48d91e2eafb10531bf3
BZ2116 <bzy1621@outlook.com>
2026-05-27 14:17:34 +0800

    feat: add router and layout components

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    component: () => import('../components/AppLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('../views/Home.vue'),
+      },
+      {
+        path: '/history',
+        name: 'History',
+        component: () => import('../views/History.vue'),
+      },
+      {
+        path: '/history/:id',
+        name: 'HistoryDetail',
+        component: () => import('../views/HistoryDetail.vue'),
+      },
+      {
+        path: '/settings',
+        name: 'Settings',
+        component: () => import('../views/Settings.vue'),
+      },
+    ],
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
